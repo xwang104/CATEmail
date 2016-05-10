1140404ed392399f1fe549a03e08018b10465e6e
Xiaoxiao Wang <xwang104@illinois.edu>
2016-05-10 02:28:57 +0000

    initial

---
+++ server.py
@@ -0,0 +1,93 @@
+import cherrypy
+import os.path
+import requests
+from urllib import urlencode
+import json
+from apiclient.discovery import build
+from oauth2client.client import OAuth2WebServerFlow
+import httplib2
+
+INTERNAL_IP = '10.140.0.2'
+EXTERNAL_IP = '104.155.193.244'
+
+clientID = '961180585767-43l499eej0riiqljcla75psc39cavghj.apps.googleusercontent.com'
+clientSecret = "TMLV_8rYxLV0vV77U-dD8vxs"
+redirectURI = 'http://www.catemail.tk/oauth2callback'
+gmailScope = 'https://www.googleapis.com/auth/gmail'
+
+class CATEmail(object):
+  @cherrypy.expose
+  def index(self):
+    return file('html/index.html')
+
+  @cherrypy.expose
+  def oauth2callback(self, code = None):
+    flow = OAuth2WebServerFlow(client_id=clientID,
+                               client_secret=clientSecret,
+                               scope=gmailScope,
+                               redirect_uri=redirectURI)
+
+    #auth_uri = flow.step1_get_authorize_url()
+    credentials = flow.step2_exchange(code)
+    http = httplib2.Http()
+    http = credentials.authorize(http)
+    gmail = build('gmail', 'v1', http=http)
+    userId = 'me'
+    maxResults = 100
+    q = "category:promotions"
+    response = gmail.users().messages().list(userId = userId, 
+                                             mexResults = maxResults,
+                                             q = q).execute()
+    
+    messages = []
+    if 'messages' in response:
+      messages.extend(response['messages'])
+
+    while 'nextPageToken' in response:
+      pageToken = response['nextPageToken']
+      response = service.users().messages().list(userId=userId, 
+                              q=q,
+                              pageToken=pageToken).execute()
+      messages.extend(response['messages'])
+
+
+    for message in messages:
+      headers = message['payload']['headers']
+      print (len(headers))
+      if len(headers) > 0:
+        print headers[0]['Subject']
+    return 'OK'
+
+    """
+    data = {'code': code,
+            'client_id': clientID,
+            'client_secret': clientSecret,
+            'redirect_uri': redirectURI,
+            'grant_type': 'authorization_code'}
+    url = 'https://accounts.google.com/o/oauth2/token'
+    print urlencode(data).encode()
+    res = requests.post(url, data)
+    print res.content
+    return 'OK: ' + urlencode(data).encode() 
+    """
+
+  @cherrypy.expose
+  @cherrypy.tools.json_out()
+  @cherrypy.tools.json_in()
+  def token(self):
+    print cherrypy.request.json
+    return {'status': 'OK'}
+
+if __name__ == '__main__':
+  current_dir = os.path.dirname(os.path.abspath(__file__))
+  cherrypy.server.socket_host = INTERNAL_IP
+  #cherrypy.config.update({'server.socket_port': 80})
+  cherrypy.server.socket_port = 80
+  #cherrypy.config.update('config')
+  cherrypy.config.update({'environment': 'production',
+                          'log.error_file': 'site.log',
+                          'log.screen': True})
+
+  conf = {'/js': {'tools.staticdir.on':  True,
+                  'tools.staticdir.dir': os.path.join(current_dir, 'js')}}
+  cherrypy.quickstart(CATEmail(), '/', config=conf)
